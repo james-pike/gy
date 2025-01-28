f90b9010812188bffa9c2168f4f295f7567e40b3
Tester <test@email.com>
2025-01-27 23:01:17 -0500

    commit

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,11 +1,10 @@
 import { component$ } from '@builder.io/qwik';
-import { PageWrapper2 } from '../common/PageWrapper2';
 import ContactForm from '../widgets/ContactForm';
 
 export default component$(() => {
   return (
-    <PageWrapper2>
+    
    <ContactForm/>
-</PageWrapper2>
+
   );
 });
